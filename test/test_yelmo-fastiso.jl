6a8be7e95f8537b899aa8937925e631d64ecfc40
Alexander Robinson <alexander.robinson@awi.de>
2026-04-17 07:03:26 +0200

    Latest test_yelmo-fastiso.jl, but FastIsostasy initialization not working well.

--- test/test_yelmo-fastiso.jl
+++ test/test_yelmo-fastiso.jl
@@ -11,6 +11,8 @@ using Oceananigans.Grids
 using Yelmo
 using FastIsostasy
 
+# Initialize Yelmo #################
+
 # Define parameters and write parameter file
 p = YelmoParameters("Greenland")
 
@@ -18,31 +20,27 @@ p = YelmoParameters("Greenland")
 ylmo = YelmoMirror(p, 0.0; rundir="run01", overwrite=true);
 
 # Initialize FastIsostasy #################
+
 W, n, T = 3f6, 7, Float32
 Nx, Ny = ylmo.g.Nx, ylmo.g.Ny
-domain = RegionalDomain(3500e3, 3500e3, Nx, Ny) 
+domain = RegionalDomain(T(3500e3), T(3500e3), Nx, Ny) 
 c = PhysicalConstants(rho_ice = ylmo.p.phys.rho_ice)
 
-bcs = BoundaryConditions(domain, ice_thickness = ExternallyUpdatedIceThickness)
+bcs = BoundaryConditions(domain, ice_thickness = ExternallyUpdatedIceThickness())
 sealevel = RegionalSeaLevel(
     surface = LaterallyVariableSeaSurface(),
-    load = InteractiveSealevelLoad(),
+    load = NoSealevelLoad(),
     bsl = PiecewiseConstantBSL(),
 )
 
-G = 0.50605f11              # Shear modulus (Pa)
-nu = 0.28f0                 # Poisson ratio
-E = G * 2 * (1 + nu)        # Young modulus
-lb = c.r_equator .- [6301f3, 5951f3, 5701f3]    # 3 layer boundaries
 solidearth = SolidEarth(
     domain,
-    layer_boundaries = T.(lb),
-    layer_viscosities = [1f21, 1f21, 2f21],
-    litho_youngmodulus = E,
-    litho_poissonratio = nu,
-    rho_litho = 3100f0,
-    rho_uppermantle = 3500f0,
 )
+solidearth.litho_rigidity .= T(1f25)
+solidearth.effective_viscosity .= T(1f21)
+
+sim1 = Simulation(domain, bcs, sealevel, solidearth, (0, 1f3))
+integrator = init_integrator(sim1::Simulation)
 
 ###########################################
 
@@ -52,21 +50,30 @@ ylmo.bnd.H_sed .= 100.0;
 # Initialize Yelmo state
 init_state!(ylmo, 0.0, "robin-cold");
 
+# Initialize output files
+yelmo_out = init_output(ylmo, joinpath(ylmo.rundir,"yelmo.nc"),
+    selection = OutputSelection(
+        groups  = [:tpo,:dyn,:thrm,:mat,:bnd],
+    )
+)
 time_init, time_end, dt = 0.0, 5.0, 1.0;
 
 for t in time_init:dt:time_end
 
     # Update isostasy
-    ylmo.bnd.z_bed .+= 100.0
+    isos.now.H_ice .= 
+    step!()
+    ylmo.bnd.z_bed .= ylmo.bnd.z_ref .+ isos.now.u .+ isos.now.ue
+    ylmo.bnd.z_sl .= isos.now.z_ss
 
     # Advance by dt
     time_step!(ylmo,t-ylmo.time);
 
-    write_output!(out, ylmo)
+    write_output!(yelmo_out, ylmo)
 end
 
 # Close output file
-close(out)
+close(yelmo_out)
 
 # Plot some data
 heatmap(ylmo.dyn.uxy_s,colorscale=log10)
